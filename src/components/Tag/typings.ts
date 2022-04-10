8dfa34ffdeaa831456cafad6e0b5d653b4f54d99
Davydx7 <oludave0511@gmail.com>
2022-04-10 14:18:33 +0100

    type update

--- src/components/Tag/typings.ts
+++ src/components/Tag/typings.ts
@@ -1,6 +1,8 @@
+import React from 'react';
+
 export type Props = {
 	label?: 'All' | 'UI' | 'UX' | 'Enhancement' | 'Bug' | 'Feature';
 	clickable: boolean;
 	isSelected: boolean;
-	onClick?: () => void;
+	onClick?: (e: React.MouseEvent<HTMLElement>) => void;
 };
